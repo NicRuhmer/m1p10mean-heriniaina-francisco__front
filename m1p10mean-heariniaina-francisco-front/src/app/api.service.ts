3cfaa5c2444b27ba66a867dcc554a2fd43193843
NoamGB <francisco@constellation-group.co>
2023-01-30 01:21:16 +0300

    ok

---
+++ m1p10mean-heariniaina-francisco-front/src/app/api.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpClientModule } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
+
+
+// import { FCM } from '@ionic-native/fcm/ngx';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ApiService {
+  getSoldeClient(idNumero: string) {
+    throw new Error('Method not implemented.');
+  }
+    user:any;
+   tab:any;
+  constructor(public http: HttpClient,private apiService: ApiService, public router: Router) { }
+
+
+
+
+ /*
+  showLoading() {  
+   this.loadingCtrl.create({ message: 'Loading.....'  }).then((loading) => {   
+       loading.present();{
+     } 
+      setTimeout(() => {   
+        loading.dismiss();  
+      }, 5000 );   
+     });  
+   }  */
+
+URL = 'http://localhost:300';
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+
+  loginClient(imput:any){
+  
+    return this.http.post(this.URL+"/api/login.user",imput,this.httpOptions);
+  }
+  
+  /*
+   getMyOffreAppel(path_,id_num:string){
+    return this.http.get(this.URL+"");
+  }
+*/
+
+}
--- m1p10mean-heariniaina-francisco-front/src/app/app.module.ts
+++ m1p10mean-heariniaina-francisco-front/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -30,7 +31,9 @@ import { ReparationComponent } from './components/reparation/reparation.componen
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
