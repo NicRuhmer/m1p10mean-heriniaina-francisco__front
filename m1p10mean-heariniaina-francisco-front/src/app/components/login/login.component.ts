858cb25d71b6bd009555442bc7c3e7994a76f6e2
NicRuhmer <nicrohmer0@gmail.com>
2023-01-30 20:21:02 +0300

    Login s

--- m1p10mean-heariniaina-francisco-front/src/app/components/login/login.component.ts
+++ m1p10mean-heariniaina-francisco-front/src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpClientModule } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
+
+// import { ApiService } from '../../api.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  data: any;
+  username: any = '';
+  password: any = '';
+  error: any;
+  path_login = "http://localhost:3000/api/login.user";
 
-  constructor() { }
+  constructor(public http: HttpClient, public activatedRoute: ActivatedRoute, public router: Router) { }
 
   ngOnInit() {
   }
+
+  submit() {
+    this.router.navigateByUrl('dashboard');
+
+  }
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+
+
+  login() {
+    const form = {
+      username: this.username,
+      password: this.password
+    };
+
+    if (this.username == null || this.username == "" || this.password == null || this.password == "") {
+      this.error = "Champ invalid";
+    } else {
+      this.error = null;
+
+      this.http.post("http://localhost:3000/api/login.user", form, this.httpOptions).subscribe((result: any) => {
+        if (result.status == 200) {
+          localStorage.setItem("id", result.data._id);
+          localStorage.setItem("name", result.data.name + " " + result.data.username);
+          this.router.navigateByUrl('/dashboard');
+        } else {
+          this.error = result.message;
+        }
+
+      });
+
+    }
+  }
+
+  deconnecte() {
+    localStorage.removeItem("id");
+    localStorage.removeItem("name");
+    this.router.navigateByUrl('/login');
+  }
+
 
 }
